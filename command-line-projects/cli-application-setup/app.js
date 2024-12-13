f3db0f6654f02a7546762cdb52da72aa34171396
divyashantkumarCS <divyashant@checkedspot.com>
2024-12-13 23:57:10 +0530

    finished basic version of Command Line Inteface application

--- command-line-projects/cli-application-setup/app.js
+++ command-line-projects/cli-application-setup/app.js
@@ -1,14 +1,19 @@
+#!/usr/bin/env node
 
-const { program } = require("commander");
+import { Command } from 'commander';
+
+const program = new Command();
 
 program.
     version("1.0.0").
     description("First CLI applicaiton setup with commander package");
 
-program.parse(process.argv);
-
 program.
     command("greet <name>").
     description("Greet the user with the given name in the command argument").
     action(name => console.log(`Hello ${name}`));
+
+    
+
 program.parse(process.argv);
+
